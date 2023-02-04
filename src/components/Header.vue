9e958a3c238ed633bcbf63a73a2570868a45758d
Victoria Stasenko <victoria.stasenko@smeetz.com>
2023-02-04 14:51:43 +0300

    started project

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header-container">
+    <div>
+       <img src="@/assets/logo.png" alt="logo" />
+    </div>
+    <button>Update products</button>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import logo from "@/assets/logo.svg"
+
+export default defineComponent({
+  name: "Header",
+  setup() {
+  //  const iconContent = logo;
+  //  const iconAlt = "logo";
+  //  const iconStyle = "width: 10%; height: 10%;";
+
+   return {
+    // iconContent,
+    // iconStyle,
+    // iconAlt,
+   }
+  },
+})
+</script>
