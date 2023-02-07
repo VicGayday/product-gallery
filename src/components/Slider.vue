d0deb2ff888b6758f56b0cc5213299e58edaa946
Victoria Stasenko <sivosha2012@gmail.com>
2023-02-07 22:26:53 +0300

    added swiper

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,14 +1,26 @@
 <template>
-  <img class="slide" :src="slides" alt="slide">
-
-
-
+<swiper
+:slides-per-view="3"
+:space-between="10"
+>
+<swiper-slide>
+   <img class="slide" :src="slides" alt="slide">
+</swiper-slide>
+</swiper>
 </template>
 
 <script>
-
+ import { Swiper, SwiperSlide } from "swiper/vue/swiper-vue";
+ import { Pagination } from 'swiper';
+ import "swiper/swiper.css";
+ import 'swiper/swiper-bundle.css';
 export default ({
   name: 'Slider',
+  components: {
+   Swiper,
+   SwiperSlide,
+   Pagination,
+  },
   props: {
     slides: {
       type: String,
